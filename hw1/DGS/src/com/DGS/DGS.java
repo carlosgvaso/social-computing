0a87bd0b886a4ca576eaf77b4b4a2436000725ca
ashleypayne <ashleypayne@ashleys-mbp-4>
2018-09-23 20:07:31 -0500

    Setting up dgs and git

--- hw1/DGS/src/com/DGS/DGS.java
+++ hw1/DGS/src/com/DGS/DGS.java
@@ -1,5 +1,33 @@
 package com.DGS;
 
+import com.KM.KM;
+
 /* 
  * HW1 EX5 - DGS Algorithm.
+ * 
  */
+
+public class DGS {
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+			DGS dgs = new DGS();
+			
+			dgs.readInputFile(args[0]);
+			dgs.initLabels();
+	}
+
+}
